d700a0328c75123b620ca1edee3fba256ba280b3
Michael Rapp <mrapp@weareratio.com>
2014-01-29 17:16:30 -0800

    Created initial solution structure referencing PRISM StoreApps for 8.1

---
+++ OneMSQFT.Windows/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Application template is documented at http://go.microsoft.com/fwlink/?LinkId=234227
+using Microsoft.Practices.Prism.StoreApps;
+using OneMSQFT.Windows.Navigation;
+
+namespace OneMSQFT.Windows
+{
+    /// <summary>
+    /// Provides application-specific behavior to supplement the default Application class.
+    /// </summary>
+    sealed partial class App : MvvmAppBase
+    {
+        protected override Task OnLaunchApplication(LaunchActivatedEventArgs args)
+        {
+            NavigationService.Navigate(ViewLocator.Pages.Timeline, null);
+            return Task.FromResult(0);
+        }        
+    }
+}
--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 ======
 
 Windows Store App and future Windows Phone application for 1MSQFT
+
+OneMSQFT.Windows_TemporaryKey
+Password: weareratio
