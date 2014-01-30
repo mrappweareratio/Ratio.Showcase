b56ef60ac05c62a2a5109331d698f98162cd10c4
aleksandrj1 <agembinski@weareratio.com>
2014-01-30 13:37:57 -0800

    MMSW-93 #1hr #comment PortableStrings inclusion

---
+++ OneMSQFT.Portable/PortableStrings.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OneMSQFT.Common
+{
+    public class PortableStrings
+    {
+        private static Strings _strings;
+        public static Strings Strings
+        {
+            get
+            {
+                if (_strings == null)
+                {
+                    _strings = new Strings();
+                }
+                return _strings;
+            }
+        }
+    }
+}
