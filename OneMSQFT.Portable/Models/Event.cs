78401dfcba6c49f848e6ac57c30536b7a8414428
Michael Rapp <mrapp@weareratio.com>
2014-01-30 16:50:32 -0800

    Creating Model and Interface classes based on initial tech spec provided MMSW-92 #time 1h

---
+++ OneMSQFT.Portable/Models/Event.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OneMSQFT.Common.Models
+{
+    public class Event : IEvent
+    {
+        public string Id { get; set; }
+        public string SocialMediaTitle { get; set; }
+        public string SocialMediaDescription { get; set; }
+        public string SocialMediaImagePath { get; set; }
+        public int SquareFootage { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Geolocation { get; set; }
+        public string DateRange { get; set; }
+        public DateTime DateStart { get; set; }
+        public DateTime DateEnd { get; set; }
+        public string SeoMetaDescription { get; set; }
+        public string PhotoFilePath { get; set; }
+        public List<Curator> Curators { get; set; }
+        public string Color { get; set; }
+    }
+}
