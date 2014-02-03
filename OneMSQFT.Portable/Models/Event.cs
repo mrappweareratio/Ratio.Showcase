3439cec761effaa219f08084a9b8bf55872ef593
Michael Rapp <mrapp@weareratio.com>
2014-02-03 15:14:07 -0800

    Updating models and interfaces with newly provided tech spec MMSW-92 #time 1.5h #comment Updating models and interfaces with newly provided tech spec

--- OneMSQFT.Portable/Models/Event.cs
+++ OneMSQFT.Portable/Models/Event.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace OneMSQFT.Common.Models
 {
-    public class Event : IEvent
+    public class Event : IEvent<Curator>
     {
         public string Id { get; set; }
         public string SocialMediaTitle { get; set; }
@@ -14,13 +14,16 @@ namespace OneMSQFT.Common.Models
         public int SquareFootage { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public string Longitude { get; set; }
+        public string Lattitude { get; set; }
         public string Geolocation { get; set; }
-        public string DateRange { get; set; }
+        public string DisplayDate { get; set; }
         public DateTime DateStart { get; set; }
         public DateTime DateEnd { get; set; }
         public string SeoMetaDescription { get; set; }
         public string PhotoFilePath { get; set; }
-        public List<Curator> Curators { get; set; }
+        public IEnumerable<Curator> Curators { get; set; }
         public string Color { get; set; }
+        public IEnumerable<MediaContentSource> MediaContent { get; private set; }
     }
 }
---
+++ OneMSQFT.Portable/Models/TimelineResult.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace OneMSQFT.Common.Models
+{
+    public class TimelineResult
+    {
+        IEnumerable<Event> Events { get; set; } 
+    }
+}
