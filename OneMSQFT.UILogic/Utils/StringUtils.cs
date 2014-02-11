0e582939d412775920804403093690e58b6b3c92
Michael Rapp <mrapp@weareratio.com>
2014-02-11 15:04:12 -0800

    StringUtils for Square Foot Parsing

---
+++ OneMSQFT.UILogic/Utils/StringUtils.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OneMSQFT.UILogic.Utils
+{
+    public class StringUtils
+    {
+        /// <summary>
+        /// Replaces spaces instead of commas
+        /// </summary>
+        public static String ToSquareFeet(int squareFeet)
+        {
+            return String.Format("{0:# ### ###}", squareFeet);
+        }
+    }
+}
