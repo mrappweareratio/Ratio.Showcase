dc0feeea94021ff38ae3a374aed1db4e21c558bf
Enrique Prado <eprado@weareratio.com>
2014-02-12 13:22:48 -0800

    Implemented and registered real InternetConnectionService

---
+++ OneMSQFT.UILogic/Services/InternetConnectionService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OneMSQFT.Common.Services;
+
+namespace OneMSQFT.UILogic.Services
+{
+    public class InternetConnectionService : IInternetConnection
+    {
+        public bool IsConnected()
+        {
+            return true;
+            //return IsConnectedInternal();
+        }
+    }
+}
