3a775085538c3417ac33470fa12f86e6576c4d7c
Enrique Prado <eprado@weareratio.com>
2014-02-13 16:39:05 -0800

    Performance improvement

--- OneMSQFT.UILogic/Services/InternetConnectionService.cs
+++ OneMSQFT.UILogic/Services/InternetConnectionService.cs
@@ -10,6 +10,9 @@ namespace OneMSQFT.UILogic.Services
 {
     public class InternetConnectionService : IInternetConnection
     {
+        private bool _statusInitialized = false;
+        private bool _isConnected = false;
+        
         public InternetConnectionService()
         {
             NetworkInformation.NetworkStatusChanged += NetworkInformation_NetworkStatusChanged;
@@ -17,20 +20,24 @@ namespace OneMSQFT.UILogic.Services
 
         public bool IsConnected()
         {
-            var prof = NetworkInformation.GetInternetConnectionProfile();
-            bool connected = false;
+            if (_statusInitialized) 
+                return this._isConnected;
 
+            var prof = NetworkInformation.GetInternetConnectionProfile();
             if (prof != null)
             {
-                connected = prof.GetNetworkConnectivityLevel() == NetworkConnectivityLevel.InternetAccess;
+                this._isConnected = prof.GetNetworkConnectivityLevel() == NetworkConnectivityLevel.InternetAccess;
+                _statusInitialized = true;
             }
-            return connected;
+
+            return this._isConnected;
         }
         
         public event EventHandler InternetConnectionChanged;
 
         private void NetworkInformation_NetworkStatusChanged(object sender)
         {
+            _statusInitialized = false;
             var arg = new InternetConnectionChangedEventArgs
                           {IsConnected = this.IsConnected()};
  
