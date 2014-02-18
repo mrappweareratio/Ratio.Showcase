d94c5ea5436cdd9d297f8676298d3b245e2a7d56
Michael Rapp <mrapp@weareratio.com>
2014-02-18 15:39:08 -0800

    Placed commented fields for turning on ApiDataRepository

--- OneMSQFT.Windows/App.xaml.cs
+++ OneMSQFT.Windows/App.xaml.cs
@@ -79,6 +79,8 @@ namespace OneMSQFT.Windows
             //register repositories
             //_container.RegisterType<IDataRepository, SampleDataRepository>(new ContainerControlledLifetimeManager());
             _container.RegisterType<IDataRepository, DemoDataRepository>(new ContainerControlledLifetimeManager());
+            //_container.RegisterType<IApiConfiguration, ApiConfiguration>(new ContainerControlledLifetimeManager());
+            //_container.RegisterType<IDataRepository, ApiDataRepository>(new ContainerControlledLifetimeManager());
 
             //register services
             _container.RegisterInstance<INavigationService>(NavigationService);
