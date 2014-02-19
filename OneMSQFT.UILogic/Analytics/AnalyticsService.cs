cbe60d712580299b64558b1c9cd0b9e1f7484c78
Michael Rapp <mrapp@weareratio.com>
2014-02-19 14:50:33 -0800

    Disabled flag in AnalyticsService Turn off when testing AnalyticsService traffic

--- OneMSQFT.UILogic/Analytics/AnalyticsService.cs
+++ OneMSQFT.UILogic/Analytics/AnalyticsService.cs
@@ -11,19 +11,24 @@ namespace OneMSQFT.UILogic.Analytics
     public class AnalyticsService : IAnalyticsService
     {
         private static readonly ADMS_Measurement _measure = ADMS_Measurement.Instance;
+        
+        private const bool Disabled = true;
 
         public void Configure()
         {
+            if (Disabled) return;
             TrackingHelper.ConfigureAppMeasurement();
         }
 
         public void StartSession()
         {
+            if (Disabled) return;
             _measure.StartSession();
         }
 
         public void StopSession()
         {
+            if (Disabled) return;
             _measure.StopSession();
         }
     }
