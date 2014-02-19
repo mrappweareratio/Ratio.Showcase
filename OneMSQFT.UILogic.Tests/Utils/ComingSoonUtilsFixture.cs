aed12fe421306c8d20a54678544b223c8136fa6c
Michael Rapp <mrapp@weareratio.com>
2014-02-19 10:40:03 -0800

    Coming Soon Updates REQ: Remainder of page blank space

--- OneMSQFT.UILogic.Tests/Utils/ComingSoonUtilsFixture.cs
+++ OneMSQFT.UILogic.Tests/Utils/ComingSoonUtilsFixture.cs
@@ -13,16 +13,16 @@ namespace OneMSQFT.UILogic.Tests.Utils
     public class ComingSoonUtilsFixture
     {
         [TestMethod]
-        public void InsertComingSoonItems_Slots_Few_No_Coming_Soon()
+        public void InsertComingSoonItemsFillingPage_Slots_Few_No_Coming_Soon()
         {
-            var events = new List<EventItemViewModel> {new EventItemViewModel(null), new EventItemViewModel(null)};
-            events = ComingSoonUtils.InsertComingSoonItems(12, events);
+            var events = new List<EventItemViewModel> { new EventItemViewModel(null), new EventItemViewModel(null) };
+            events = ComingSoonUtils.InsertComingSoonItemsFillingPage(12, events);
             Assert.AreEqual(events.Count, 12, "Fills Page");
             Assert.AreEqual(events.Count(x => x is ComingSoonFakeEventItemViewModel), 10, "Coming Soon");
         }
 
         [TestMethod]
-        public void InsertComingSoonItems_Slots_Coming_Soon_One_Page()
+        public void InsertComingSoonItemsFillingPage_Slots_Coming_Soon_One_Page()
         {
             var events = new List<EventItemViewModel>
             {
@@ -36,7 +36,7 @@ namespace OneMSQFT.UILogic.Tests.Utils
                 new EventItemViewModel(null), 
                 new EventItemViewModel(null)
             };
-            events = ComingSoonUtils.InsertComingSoonItems(12, events);
+            events = ComingSoonUtils.InsertComingSoonItemsFillingPage(12, events);
             Assert.AreEqual(events.Count, 12, "Fills Page");
             Assert.AreEqual(events.Count(x => x is ComingSoonFakeEventItemViewModel), 4, "Coming Soon");
             foreach (var index in ComingSoonUtils.SlotsDictionaryByPageSize[12])
@@ -46,7 +46,7 @@ namespace OneMSQFT.UILogic.Tests.Utils
         }
 
         [TestMethod]
-        public void InsertComingSoonItems_Slots_Coming_Soon_Two_Pages()
+        public void InsertComingSoonItemsFillingPage_Slots_Coming_Soon_Two_Pages()
         {
             var events = new List<EventItemViewModel>
             {
@@ -68,7 +68,7 @@ namespace OneMSQFT.UILogic.Tests.Utils
                 new EventItemViewModel(null), 
                 new EventItemViewModel(null)
             };
-            events = ComingSoonUtils.InsertComingSoonItems(12, events);
+            events = ComingSoonUtils.InsertComingSoonItemsFillingPage(12, events);
             Assert.AreEqual(events.Count, 24, "Two Pages");
             Assert.AreEqual(events.Count(x => x is ComingSoonFakeEventItemViewModel), 8, "Coming Soon Count");
             foreach (var index in ComingSoonUtils.SlotsDictionaryByPageSize[12])
@@ -82,7 +82,7 @@ namespace OneMSQFT.UILogic.Tests.Utils
         }
 
         [TestMethod]
-        public void InsertComingSoonItems_Slots_Coming_Soon_Two_Pages_Near_Full()
+        public void InsertComingSoonItemsFillingPage_Slots_Coming_Soon_Two_Pages_Near_Full()
         {
             var events = new List<EventItemViewModel>
             {
@@ -102,7 +102,7 @@ namespace OneMSQFT.UILogic.Tests.Utils
                 new EventItemViewModel(null), 
                 new EventItemViewModel(null)                 
             };
-            events = ComingSoonUtils.InsertComingSoonItems(12, events);
+            events = ComingSoonUtils.InsertComingSoonItemsFillingPage(12, events);
             Assert.AreEqual(events.Count, 24, "Two Pages");
             Assert.AreEqual(events.Count(x => x is ComingSoonFakeEventItemViewModel), 10, "Coming Soon Count");
             foreach (var index in ComingSoonUtils.SlotsDictionaryByPageSize[12])
@@ -112,6 +112,130 @@ namespace OneMSQFT.UILogic.Tests.Utils
             foreach (var index in ComingSoonUtils.SlotsDictionaryByPageSize[12])
             {
                 Assert.IsTrue(events[index + 12] is ComingSoonFakeEventItemViewModel, String.Format("Coming Soon @ {0}", index + 12));
+            }
+        }
+
+        [TestMethod]
+        public void Events_Two_Only()
+        {
+            var events = new List<EventItemViewModel>
+            {
+                //2
+                new EventItemViewModel(null), 
+               new EventItemViewModel(null)
+            };
+            events = ComingSoonUtils.InsertComingSoonItems(12, events);
+            Assert.AreEqual(events.Count, 2, "Two Events");
+        }
+
+        [TestMethod]
+        public void Events_Three()
+        {
+            var events = new List<EventItemViewModel>
+            {
+                //3
+                new EventItemViewModel(null), 
+                new EventItemViewModel(null), 
+               new EventItemViewModel(null)
+            };
+            events = ComingSoonUtils.InsertComingSoonItems(12, events);
+            Assert.AreEqual(events.Count, 5, "Two Events");
+            foreach (var index in ComingSoonUtils.SlotsDictionaryByPageSize[12])
+            {
+                if (index < events.Count)
+                    Assert.IsTrue(events[index] is ComingSoonFakeEventItemViewModel, String.Format("Coming Soon @ {0}", index));
+            }
+        }
+
+        [TestMethod]
+        public void Events_Page()
+        {
+            var events = new List<EventItemViewModel>
+            {
+                //8
+                new EventItemViewModel(null), 
+                new EventItemViewModel(null), 
+               new EventItemViewModel(null),
+               new EventItemViewModel(null),
+               new EventItemViewModel(null),
+               new EventItemViewModel(null),
+               new EventItemViewModel(null),
+               new EventItemViewModel(null)
+            };
+            events = ComingSoonUtils.InsertComingSoonItems(12, events);
+            Assert.AreEqual(events.Count, 12, "Total Count");            
+            Assert.AreEqual(events.Count(x => x is ComingSoonFakeEventItemViewModel), 4, "Coming Soon Count");
+            foreach (var index in ComingSoonUtils.SlotsDictionaryByPageSize[12])
+            {
+                if (index < events.Count)
+                    Assert.IsTrue(events[index] is ComingSoonFakeEventItemViewModel, String.Format("Coming Soon @ {0}", index));
+            }
+        }
+
+        [TestMethod]
+        public void Events_Page_With_2()
+        {
+            var events = new List<EventItemViewModel>
+            {
+                //10
+                new EventItemViewModel(null), 
+                new EventItemViewModel(null), 
+               new EventItemViewModel(null),
+               new EventItemViewModel(null),
+               new EventItemViewModel(null),
+               new EventItemViewModel(null),
+               new EventItemViewModel(null),
+               new EventItemViewModel(null),
+               new EventItemViewModel(null),
+               new EventItemViewModel(null)
+            };
+            events = ComingSoonUtils.InsertComingSoonItems(12, events);
+            Assert.AreEqual(events.Count, 14, "Total Count");
+            Assert.AreEqual(events.Count(x => x is ComingSoonFakeEventItemViewModel), 4, "Coming Soon Count");
+            foreach (var index in ComingSoonUtils.SlotsDictionaryByPageSize[12])
+            {
+                if (index < events.Count)
+                    Assert.IsTrue(events[index] is ComingSoonFakeEventItemViewModel, String.Format("Coming Soon @ {0}", index));
+            }
+            foreach (var index in ComingSoonUtils.SlotsDictionaryByPageSize[12])
+            {
+                var i = index + 12;
+                if (i < events.Count)
+                    Assert.IsTrue(events[i] is ComingSoonFakeEventItemViewModel, String.Format("Coming Soon @ {0}", i));
+            }
+        }
+
+        [TestMethod]
+        public void Events_Page_With_3()
+        {
+            var events = new List<EventItemViewModel>
+            {
+                //11
+                new EventItemViewModel(null), 
+                new EventItemViewModel(null), 
+               new EventItemViewModel(null),
+               new EventItemViewModel(null),
+               new EventItemViewModel(null),
+               new EventItemViewModel(null),
+               new EventItemViewModel(null),
+               new EventItemViewModel(null),
+               new EventItemViewModel(null),
+               new EventItemViewModel(null),
+               new EventItemViewModel(null)
+            };
+            events = ComingSoonUtils.InsertComingSoonItems(12, events);
+            Assert.AreEqual(events.Count, 17, "Total Count");
+            Assert.AreEqual(events.Count(x => x is ComingSoonFakeEventItemViewModel), 6, "Coming Soon Count");
+            foreach (var index in ComingSoonUtils.SlotsDictionaryByPageSize[12])
+            {
+                if (index < events.Count)
+                    Assert.IsTrue(events[index] is ComingSoonFakeEventItemViewModel, String.Format("Coming Soon @ {0}", index));
+            }
+            foreach (var index in ComingSoonUtils.SlotsDictionaryByPageSize[12])
+            {
+                var i = index + 12;
+                if (i < events.Count)
+                    Assert.IsTrue(events[i] is ComingSoonFakeEventItemViewModel, String.Format("Coming Soon @ {0}", i));
             }
         }
     }
--- OneMSQFT.UILogic/Utils/ComingSoonUtils.cs
+++ OneMSQFT.UILogic/Utils/ComingSoonUtils.cs
@@ -14,13 +14,7 @@ namespace OneMSQFT.UILogic.Utils
            {12, new []{2,3,8,10}}
         };
 
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="pageSize"></param>
-        /// <param name="events"></param>
-        /// <returns></returns>
-        public static List<EventItemViewModel> InsertComingSoonItems(int pageSize, List<EventItemViewModel> events)
+        public static List<EventItemViewModel> InsertComingSoonItemsFillingPage(int pageSize, List<EventItemViewModel> events)
         {
             if (!SlotsDictionaryByPageSize.ContainsKey(pageSize))
             {
@@ -40,6 +34,35 @@ namespace OneMSQFT.UILogic.Utils
                 else if (i > events.Count - 1)
                 {
                     events.Add(new ComingSoonFakeEventItemViewModel());
+                }
+                else
+                {
+                    continue;
+                }
+            }
+            return events;
+        }
+
+        public static List<EventItemViewModel> InsertComingSoonItems(int pageSize, List<EventItemViewModel> events)
+        {
+            if (!SlotsDictionaryByPageSize.ContainsKey(pageSize))
+            {
+                throw new ArgumentOutOfRangeException("pageSize");
+            }
+            var eventsPerPage = pageSize - SlotsDictionaryByPageSize[pageSize].Length;
+            var pages = Math.Ceiling((decimal)events.Count / eventsPerPage);
+            var slots = pages * pageSize;
+            for (var i = 0; i < slots; i++)
+            {
+                if (i > events.Count - 1)
+                {
+                    continue;
+                }
+                var isEmpty = SlotsDictionaryByPageSize[pageSize].Contains(i % pageSize);
+                if (isEmpty)
+                {
+                    //If index is equal to Count, item is added to the end of List<T>.
+                    events.Insert(i, new ComingSoonFakeEventItemViewModel());
                 }
                 else
                 {
