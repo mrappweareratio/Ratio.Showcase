5183177b6a54c9f9c6a5f56c07a62e791dd1b427
Michael Rapp <mrapp@weareratio.com>
2014-02-20 14:46:54 -0800

    Insert fake home item into app bar list based on date MMSW-239 #resolve

--- OneMSQFT.Windows/Views/BasePageView.cs
+++ OneMSQFT.Windows/Views/BasePageView.cs
@@ -19,7 +19,7 @@ using OneMSQFT.Windows.DesignViewModels;
 
 namespace OneMSQFT.Windows.Views
 {
-    public class BasePageView : VisualStateAwarePage
+    public abstract class BasePageView : VisualStateAwarePage
     {
         public DelegateCommand<String> TopAppBarEventButtonCommand { get; set; }
         public DelegateCommand HomeButtonClickCommand { get; set; }
@@ -93,28 +93,28 @@ namespace OneMSQFT.Windows.Views
             BottomAppBar.Visibility = Visibility.Visible;
         }
 
-        public virtual void TopAppBarEventButtonCommandHandler(String eventId)
-        {
-        }
+        public abstract void TopAppBarEventButtonCommandHandler(String eventId);      
 
-        async public void HomeButtonClickCommandHandler()
+        public void HomeButtonClickCommandHandler()
         {
             Frame.Navigate(typeof(TimelinePage));
             TopAppBar.IsOpen = false;
             BottomAppBar.IsOpen = false;
         }
 
-        async public void AboutButtonClickCommandHandler()
+        public void AboutButtonClickCommandHandler()
         {
             Frame.Navigate(typeof(AboutPage));
             TopAppBar.IsOpen = false;
             BottomAppBar.IsOpen = false;
         }
-        async public virtual void FilterButtonClickCommandHandler()
+
+        public virtual void FilterButtonClickCommandHandler()
         {
             // overridden in local page
         }
-        async public virtual void AdminButtonClickCommandHandler(Button sender)
+
+        public virtual void AdminButtonClickCommandHandler(Button sender)
         {
             BottomAppBar.IsOpen = false;
             TopAppBar.IsOpen = false;
@@ -126,7 +126,7 @@ namespace OneMSQFT.Windows.Views
             homeButton.Style = (Style)App.Current.Resources["OMSQFTAppBarHomeButtonStyle"];
             homeButton.Command = TopAppBarEventButtonCommand;
             homeButton.CommandParameter = null;
-            TopAppBarContentStackPanel.Children.Clear();
+            TopAppBarContentStackPanel.Children.Clear();            
             foreach (var e in vm.SquareFootEvents)
             {
                 var b = new Button();
@@ -137,7 +137,10 @@ namespace OneMSQFT.Windows.Views
                 b.Style = (Style)App.Current.Resources["OMSQFTAppBarButtonStyle"];
                 b.DataContext = e;
                 TopAppBarContentStackPanel.Children.Add(b);
-            }
+            }            
+            var ev = vm.SquareFootEvents.FirstOrDefault(x => x.IsInThePast.GetValueOrDefault());
+            var evIndex = ev == null ? 0 : vm.SquareFootEvents.IndexOf(ev);
+            TopAppBarContentStackPanel.Children.Insert(evIndex, homeButton);
         }
 
         public T GetDataContextAsViewModel<T>() where T : INotifyPropertyChanged
