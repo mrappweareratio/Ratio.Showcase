30707589ab6b96e7c665d6aa67eb0640d0a1e1c9
Michael Rapp <mrapp@weareratio.com>
2014-02-21 14:19:07 -0800

    Missed Save

--- OneMSQFT.UILogic/Utils/PinningUtils.cs
+++ OneMSQFT.UILogic/Utils/PinningUtils.cs
@@ -35,21 +35,4 @@ namespace OneMSQFT.UILogic.Utils
             return new PinningContext();
         }
     }
-
-    public class PinningContext
-    {
-        public PinningContext(StartupItemType startupItemType = StartupItemType.None)
-        {
-            StartupItemType = startupItemType;
-        }
-
-        public PinningContext(StartupItemType startupItemType, string startupItemId)
-        {
-            StartupItemType = startupItemType;
-            StartupItemId = startupItemId;
-        }
-
-        public StartupItemType StartupItemType { get; private set; }
-        public string StartupItemId { get; private set; }
-    }
 }
