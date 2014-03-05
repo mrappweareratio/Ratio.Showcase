dd7fa4636814801ce36366a9b403ccd482d29191
Michael Rapp <mrapp@weareratio.com>
2014-03-04 19:53:20 -0800

    RetryHandler for HttpMessageHandler Retries the maximum amount of times based on configuration. TryCatchAsync on the SendAsync allows for conditional throw based on stage in retries

--- OneMSQFT.UILogic/DataLayer/ApiDataRepository.cs
+++ OneMSQFT.UILogic/DataLayer/ApiDataRepository.cs
@@ -63,33 +63,4 @@ namespace OneMSQFT.UILogic.DataLayer
             return new Uri(url, UriKind.Absolute);
         }
     }
-
-    public class RetryHandler : DelegatingHandler
-    {
-        public int MaxRetries { get; set; }
-
-        public RetryHandler(HttpMessageHandler innerHandler, int maxRetries)
-            : base(innerHandler)
-        {
-            MaxRetries = maxRetries;
-        }        
-
-        protected override async Task<HttpResponseMessage> SendAsync(
-            HttpRequestMessage request,
-            CancellationToken cancellationToken)
-        {
-            HttpResponseMessage response = null;
-
-            for (var i = 0; i < MaxRetries; i++)
-            {
-                response = await base.SendAsync(request, cancellationToken);
-                if (response.IsSuccessStatusCode)
-                {
-                    return response;
-                }
-            }
-
-            return response;
-        }
-    }
 }
